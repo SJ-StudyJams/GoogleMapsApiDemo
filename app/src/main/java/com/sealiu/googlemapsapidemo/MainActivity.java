86e77d0da08c59c575363a6bd7359b899c96173f
sealiu <sealiu0217@gmail.com>
2016-07-11 11:36:11 +0800

    移动镜头到指定位置，并添加maker

--- app/src/main/java/com/sealiu/googlemapsapidemo/MainActivity.java
+++ app/src/main/java/com/sealiu/googlemapsapidemo/MainActivity.java
@@ -1,33 +1,71 @@
 package com.sealiu.googlemapsapidemo;
 
+import android.Manifest;
 import android.app.Dialog;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.ActionBar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.RelativeLayout;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    GoogleMap mMap;
+    private static final String TAG = "MainActivity";
+    private static final int PERMISSIONS_REQUEST_FINE_LOCATION = 1;
+    private GoogleMap mMap;
+    private RelativeLayout tipsHolderView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setElevation(0);
-            actionBar.setTitle(R.string.app_name);
-        }
+
+        setContentView(R.layout.activity_main);
+        tipsHolderView = (RelativeLayout) findViewById(R.id.content_holder);
 
         if (isServicesOK()) {
-            setContentView(R.layout.activity_main);
-            Snackbar.make(getWindow().getDecorView(), "成功连接 Google Play Services", Snackbar.LENGTH_LONG).show();
+            Snackbar.make(tipsHolderView, "成功连接 Google Play Services",
+                    Snackbar.LENGTH_LONG).show();
+
+            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng sydney = new LatLng(-34, 151);
+
+        // 申请权限
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+            mMap.getUiSettings().setZoomControlsEnabled(true);
+
+            gotoLocation(sydney, 15, false, "Marker in Sydney");
+
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_FINE_LOCATION);
         }
     }
 
@@ -35,12 +73,27 @@ public class MainActivity extends AppCompatActivity {
         int isAvailable = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
         if (isAvailable == ConnectionResult.SUCCESS) {
             return true;
-        }else if (GooglePlayServicesUtil.isUserRecoverableError(isAvailable)) {
+        } else if (GooglePlayServicesUtil.isUserRecoverableError(isAvailable)) {
             Dialog dialog = GooglePlayServicesUtil.getErrorDialog(isAvailable, this, 0);
             dialog.show();
         } else {
-            Snackbar.make(getWindow().getDecorView(), "无法连接 Google Play Services", Snackbar.LENGTH_LONG).show();
+            Snackbar.make(tipsHolderView, "无法连接 Google Play Services",
+                    Snackbar.LENGTH_LONG).show();
         }
         return false;
     }
+
+    private void gotoLocation(LatLng ll, float zoom, boolean animate) {
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(ll, zoom);
+        if (animate)
+            mMap.animateCamera(update);
+        else
+            mMap.moveCamera(update);
+    }
+
+    private void gotoLocation(LatLng ll, float zoom, boolean animate, String makerTitle) {
+        gotoLocation(ll, zoom, animate);
+        mMap.addMarker(new MarkerOptions().position(ll).title(makerTitle));
+    }
+
 }
